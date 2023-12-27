0e624fe7b4ab22037a8d39cb22a80535ba4a3cc8
Deep <deepghosal445@gmail.com>
2023-12-27 15:45:29 +0530

    change in frontend

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import react from "react";
+import AboutCard from "@/components/aboutCard";
+
+
+const about= () => {
+    return (
+        <div className="about-background">
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+            <AboutCard />
+        </div>
+    )
+}
